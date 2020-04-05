c1455de55963c21c2c6ffb4b85708bbe8bbb488e
Matt <>
2020-04-04 21:26:44 -0500

    Add spaceauth action

---
+++ ios/fastlane/actions/spaceauth.rb
@@ -0,0 +1,64 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      SPACEAUTH_COOKIE = :SPACEAUTH_COOKIE
+    end
+
+    class SpaceauthAction < Action
+      def self.run(params)
+        result, status = Open3.capture2e("bundle exec fastlane spaceauth")
+        cookie_export_cmd = result.split("Example:")[1].strip!
+        cookie = cookie_export_cmd.split("export FASTLANE_SESSION=")[1]
+
+        Actions.lane_context[SharedValues::SPACEAUTH_COOKIE] = cookie
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "A short description with <= 80 characters of what this action does"
+      end
+
+      def self.details
+        # Optional:
+        # this is your chance to provide a more detailed description of this action
+        "You can use this action to do cool things..."
+      end
+
+      def self.available_options
+        # Define all options your action supports. 
+      end
+
+      def self.output
+        # Define the shared values you are going to provide
+        [
+          ['SPACEAUTH_COOKIE', 'The cookie parsed form the output of $ fastlane spaceauth, to be set as the FASTLANE_SESSION environment variable.']
+        ]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.authors
+        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
+        ["mjinkens1"]
+      end
+
+      def self.is_supported?(platform)
+        # you can do things like
+        # 
+        #  true
+        # 
+        #  platform == :ios
+        # 
+        #  [:ios, :mac].include?(platform)
+        # 
+
+        platform == :ios
+      end
+    end
+  end
+end
